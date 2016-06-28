f80f0a3b0d0c7e2e1ad0a3da585082fe216ca027
Rob Misiorowski <rmisio@yahoo.com>
2016-06-28 15:06:04 -0700

    updated browser sync to not fail to load on JS exception

--- bin/processIndex.js
+++ bin/processIndex.js
@@ -1,27 +1,26 @@
 const fs = require('fs');
-const pp = require('preprocess');
-
 const indexIn = `${__dirname}/../index.html`;
 const indexOut = `${__dirname}/../.tmp/index.html`;
-const browserSyncClient = `${__dirname}/../node_modules/browser-sync-client/dist/index.min.js`;
-let bsClientScript;
+const bsClientScriptPath = `${__dirname}/../js/lib/bs-client.js`;
 let indexHtml;
-
-try {
-  bsClientScript = fs.readFileSync(browserSyncClient);
-} catch (e) {
-  throw new Error(`Unable to read the browser-sync client script at ${browserSyncClient}. ${e}`);
-}
+let bsClientScript;
 
 try {
   indexHtml = fs.readFileSync(indexIn);
-
-  try {
-    fs.writeFileSync(indexOut,
-      pp.preprocess(indexHtml, { bsClientScript, NODE_ENV: process.env.NODE_ENV }));
-  } catch (e) {
-    throw new Error(`Unable to write to ${indexOut}. ${e}`);
-  }
 } catch (e) {
   throw new Error(`Unable to read ${indexIn}. ${e}`);
+}
+
+try {
+  bsClientScript = fs.readFileSync(bsClientScriptPath);
+} catch (e) {
+  throw new Error(`Unable to read ${bsClientScript}. ${e}`);
+}
+
+try {
+  const content = String(indexHtml).replace('<!-- NODE_ENV -->', process.env.NODE_ENV)
+    .replace('<!-- BROWSER_SYNC_PLACEHOLDER (DO NOT REMOVE OR ALTER!) -->', String(bsClientScript));
+  fs.writeFileSync(indexOut, content);
+} catch (e) {
+  throw new Error(`Unable to write to ${indexOut}. ${e}`);
 }
