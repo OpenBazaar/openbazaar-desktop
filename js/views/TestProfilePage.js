bf74f302fdb8e750998ae12f9332520412939bd3
Rob Misiorowski <rmisio@yahoo.com>
2016-07-21 17:36:25 -0700

    continuing to build profile test page

--- js/views/TestProfilePage.js
+++ js/views/TestProfilePage.js
@@ -1,6 +1,7 @@
+import $ from 'jquery';
 import BaseVw from './baseVw';
 import loadTemplate from '../utils/loadTemplate';
-import app from '../app';
+// import app from '../app';
 
 export default class extends BaseVw {
   constructor(options = {}) {
@@ -10,12 +11,35 @@ export default class extends BaseVw {
 
   events() {
     return {
+      'click input[type=submit]': 'onSaveForm',
     };
+  }
+
+  className() {
+    return 'testProfilePage';
+  }
+
+  onSaveForm(e) {
+    e.preventDefault();
+    this.saveForm();
+  }
+
+  saveForm() {
+    this.getFormData();
+  }
+
+  getFormData() {
+    this.$fields = this.$fields || this.$('input[name], textarea[name]');
+    this.$fields.each((index, field) => {
+      const $field = $(field);
+      console.log(`${$field.attr('name')}: ${field.value}`);
+    });
   }
 
   render() {
     loadTemplate('testProfile.html', (t) => {
       this.$el.html(t());
+      this.$fields = null;
     });
 
     return this;
