f6aa20f15248abb315181519af2b5403559c305a
Josh Jeffryes <jjeffryes@gmail.com>
2016-07-25 11:35:33 -0400

    Update PageNav styles. Consolidate spacing variables to use $pad.

--- js/views/modals/Settings/Settings.js
+++ js/views/modals/Settings/Settings.js
@@ -53,7 +53,7 @@ export default class extends BaseModal {
         this[tabViewName] = new this.tabViews[tabViewName]();
         this.tabElCache[tabViewName] = this[tabViewName].render().$el;
       }
-      this.tabContent.append(this.tabElCache[tabViewName]);
+      this.$tabContent.append(this.tabElCache[tabViewName]);
       this.currentTabView = this[tabViewName];
       this.currentTabName = tabViewName;
     }
@@ -81,7 +81,7 @@ export default class extends BaseModal {
 
       super.render();
 
-      this.tabContent = this.$('.js-tabContent');
+      this.$tabContent = this.$('.js-tabContent');
       this.selectTab(this.$('.js-tab[data-tab="SettingsGeneral"]'));
     });
 
