9256ea0cbe3ab5548174ad46e6b861c08eb9b65c
Rob Misiorowski <rmisio@yahoo.com>
2016-08-12 15:06:52 -0700

    having test create the tmp folder if necessary

--- test/_setup.js
+++ test/_setup.js
@@ -1,4 +1,11 @@
+import path from 'path';
 import fs from 'fs';
+
+const tmpFolderPath = path.join(__dirname, '../../.tmp');
+
+if (!fs.existsSync(tmpFolderPath)) {
+  fs.mkdirSync(tmpFolderPath);
+}
 
 const indexPage = fs.readFileSync(`${__dirname}/../index.html`);
 
