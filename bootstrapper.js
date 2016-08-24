5ee5514a5c8b6e0c88122b3dbda26b1de56b97e9
Rob Misiorowski <rmisio@yahoo.com>
2016-08-24 16:26:15 -0700

    added in npm build script

--- bootstrapper.js
+++ bootstrapper.js
@@ -1,3 +1,6 @@
 // install babel hooks in the main process
-require('babel-register');
+if (process.env.NODE_ENV === 'development') {
+  require('babel-register');  // eslint-disable-line global-require
+}
+
 require('./main.js');
