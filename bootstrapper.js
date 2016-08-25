9445458c91b542c83bf8c371046365c19fad1adb
Brian Hoffman <brianchoffman@gmail.com>
2016-08-25 14:35:22 -0400

    reference new mainBuilt.js

--- bootstrapper.js
+++ bootstrapper.js
@@ -1,6 +1,7 @@
 // install babel hooks in the main process
 if (process.env.NODE_ENV === 'development') {
   require('babel-register');  // eslint-disable-line global-require
+  require('./main.js');
+} else {
+  require('./mainBuilt.js');
 }
-
-require('./main.js');
