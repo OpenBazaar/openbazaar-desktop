f978983dc0c73d0d7c006f38b9079730e1e320cc
Rob Misiorowski <rmisio@yahoo.com>
2016-09-08 13:09:08 -0700

    removed unused files

--- js/collections/PhotoUploads.js
+++
@@ -1,8 +0,0 @@
-import { Collection } from 'backbone';
-import PhotoUpload from '../models/PhotoUpload';
-
-export default class extends Collection {
-  model(attrs, options) {
-    return new PhotoUpload(attrs, options);
-  }
-}
