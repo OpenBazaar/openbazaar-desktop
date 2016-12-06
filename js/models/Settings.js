a231354d874c96099c9b1167f2152e83629db51e
human10 <redacted_user@redacted>
2016-12-06 07:25:50 +0000

    Deleted the views and templates for advanced sections
      The functionality these provided is now provided by a single
      Advanced.js view and a single advanced.html template.
    
      Commits that reference parts of the discussion concerning the
      decision for a single Advanced view have already been applied.
    
      See, for example, https://github.com/OpenBazaar/openbazaar-desktop/commit/7e843983bd8a1f6bbca33683ea80123364dbbb77

--- js/models/Settings.js
+++ js/models/Settings.js
@@ -34,33 +34,6 @@ export default class extends BaseModel {
     };
   }
 
-  parse( response ) {
-    const data = this.fromAPIFormatJSON( response );
-    // make it immutable to highlight unintended modification errors
-    Object.freeze( data );
-    return data;
-  }
-
-  toModelFormatJSON( ) {
-    return SettingsAPIAdaptor.convertAPIToModelFormat( super.toJSON( ) );
-  }
-
-  toAPIFormatJSON( ) {
-    const raw = super.toJSON( );
-    return SettingsAPIAdaptor.convertModelToAPIFormat( raw );
-  }
-
-  toJSON( ) {
-    const apiFormat = this.toAPIFormatJSON( );
-    // make it immutable to highlight unintended modification errors
-    Object.freeze( apiFormat );
-    return apiFormat;
-  }
-
-  fromAPIFormatJSON( response ) {
-    return SettingsAPIAdaptor.convertAPIToModelFormat( response );
-  }
-
   sync(method, model, options) {
     if (method === 'create' && typeof options.type === 'undefined') {
       // we will use PUT unless you explicitly save with POST,
