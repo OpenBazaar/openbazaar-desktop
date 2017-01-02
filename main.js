fe07ed5b1f06204e30077cfa35f3f13ca4d3ce24
Rob Misiorowski <rmisio@yahoo.com>
2017-01-02 09:04:08 -0800

    reworked the debug log modal to use the new style

--- main.js
+++ main.js
@@ -76,6 +76,7 @@ if (handleStartupEvent()) {
 }
 
 const serverPath = `${__dirname}${path.sep}..${path.sep}OpenBazaar-Server${path.sep}`;
+// const serverPath = `${__dirname}${path.sep}..${path.sep}test-server${path.sep}`;
 const serverFilename = process.platform === 'darwin' || process.platform === 'linux' ?
       'openbazaard' : 'openbazaard.exe';
 const isBundledApp = _.once(() => fs.existsSync(serverPath + path.sep + serverFilename));
