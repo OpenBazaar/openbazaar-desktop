0bae5934177abd42da6fabe848ee56cc320f6ca3
human10 <redacted_user@redacted>
2017-01-05 11:22:21 +0000

    Sketched copy BTC address facility
      Copy on click address
      Copy on click "Copy Address" button
      Display notification "Copied"
      Clear notification after a timeout
      Clear any timeouts before a new timeout is set
      Style address and notification

--- js/views/modals/About/Donations.js
+++ js/views/modals/About/Donations.js
@@ -1,8 +1,10 @@
 import loadTemplate from '../../../utils/loadTemplate';
 import baseVw from '../../baseVw';
+import { clipboard } from 'electron';
 
 const obDonationAddress = '3MXYUBLWNETa5HTewZp1xMTt7AW9kbFNqs';
 const donationCountFloor = 500;
+let hiderTimer;
 
 export default class extends baseVw {
   constructor(options = {}) {
@@ -10,6 +12,31 @@ export default class extends baseVw {
       className: 'aboutDonations',
       ...options,
     });
+  }
+
+  events() {
+    return {
+      'click #copyAddress': 'copyDonationAddress',
+      'click #donationAddress': 'copyDonationAddress',
+    };
+  }
+
+  copyDonationAddress(e) {
+    const address = document.querySelector('#donationAddress');
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNodeContents(address);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    clipboard.writeText(obDonationAddress);
+
+    this.$('#copyNotification').addClass('active');
+    if (!!hiderTimer) {
+      clearTimeout(hiderTimer); 
+    }
+    hiderTimer = setTimeout(
+      () => this.$('#copyNotification').removeClass('active'), 3000);
   }
 
   render() {
