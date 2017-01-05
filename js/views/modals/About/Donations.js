d13904dbd487cde2858c53d09944b56b0c79fb24
human10 <redacted_user@redacted>
2017-01-05 07:02:10 +0000

    Donation count is dynamic

--- js/views/modals/About/Donations.js
+++ js/views/modals/About/Donations.js
@@ -2,6 +2,7 @@ import loadTemplate from '../../../utils/loadTemplate';
 import baseVw from '../../baseVw';
 
 const obDonationAddress = '3MXYUBLWNETa5HTewZp1xMTt7AW9kbFNqs';
+const donationCountFloor = 500;
 
 export default class extends baseVw {
   constructor(options = {}) {
@@ -15,6 +16,7 @@ export default class extends baseVw {
     loadTemplate('modals/about/donations.html', (t) => {
       this.$el.html(t({
         obDonationAddress,
+        donationCountFloor,
       }));
     });
 
