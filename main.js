be3cc39c4baa923dbfa4c66738ad1f8b004c65c0
Brian Hoffman <brianchoffman@gmail.com>
2017-01-12 11:40:48 -0500

    Change the bundled server location
    Currently it references the 1.x location folder.

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ if (handleStartupEvent()) {
   console.log('OpenBazaar started on Windows...');
 }
 
-const serverPath = `${__dirname}${path.sep}..${path.sep}OpenBazaar-Server${path.sep}`;
+const serverPath = `${__dirname}${path.sep}..${path.sep}openbazaar-go${path.sep}`;
 const serverFilename = process.platform === 'darwin' || process.platform === 'linux' ?
       'openbazaard' : 'openbazaard.exe';
 const isBundledApp = _.once(() => fs.existsSync(serverPath + path.sep + serverFilename));
