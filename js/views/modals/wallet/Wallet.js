049900ec5852ac9db8c729253f2e651a00b70aa2
Rob Misiorowski <rmisio@yahoo.com>
2017-03-23 16:42:55 -0700

    markup and styling of the wallet stats area

--- js/views/modals/wallet/Wallet.js
+++ js/views/modals/wallet/Wallet.js
@@ -1,5 +1,5 @@
 // import $ from 'jquery';
-// import app from '../../../app';
+import app from '../../../app';
 import loadTemplate from '../../../utils/loadTemplate';
 import BaseModal from '../BaseModal';
 import BTCTicker from '../../BTCTicker';
@@ -16,7 +16,7 @@ export default class extends BaseModal {
   }
 
   className() {
-    return `${super.className()} wallet modalScrollPage`;
+    return `${super.className()} wallet modalScrollPage modalMedium`;
   }
 
   events() {
@@ -31,11 +31,27 @@ export default class extends BaseModal {
     // super.close();
   // }
 
+  // Normally, we'd user the currency module to format currency, but
+  // this is a unique case where we want to format a BTC price without
+  // the BTC symbol, so we'll create a custom function. If we find other
+  // areas in the app need this, we can integrate it into the currency module.
+  formatUnitlessBtc(amount) {
+    if (typeof amount !== 'number') {
+      throw new Error('Please provide a number.');
+    }
+
+    return new Intl.NumberFormat(app.settings.get('language'), {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 8,
+    }).format(amount);
+  }
+
   render() {
     loadTemplate('modals/wallet/wallet.html', t => {
       loadTemplate('walletIcon.svg', (walletIconTmpl) => {
         this.$el.html(t({
           walletIconTmpl,
+          formatUnitlessBtc: this.formatUnitlessBtc,
         }));
 
         super.render();
