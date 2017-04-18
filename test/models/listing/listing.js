cfc28a6a871de14e0ce6c233b0fd2097ff445a75
Josh Jeffryes <jjeffryes@gmail.com>
2017-04-18 16:19:10 -0400

    Update listing test to match server change.

--- test/models/listing/listing.js
+++ test/models/listing/listing.js
@@ -29,7 +29,7 @@ describe('the Listing model', () => {
   });
 
   it('throws an error if you attempt to fetch without a slug set' +
-    ' on the nested listing model', () => {
+      ' on the nested listing model', () => {
     const listing = new Listing({}, { guid: '12345' });
 
     let errorThrown = false;
@@ -62,40 +62,38 @@ describe('the Listing model', () => {
   it('converts fiat prices from integer to decimal format in parse', () => {
     const listing = new Listing({}, { guid: '12345' });
     const parsed = listing.parse({
-      vendorListings: [
-        {
-          metadata: {
-            pricingCurrency: 'USD',
-          },
-          item: {
-            price: 123,
-          },
-          shippingOptions: [
-            {
-              services: [
-                {
-                  price: 123,
-                },
-                {
-                  price: 234,
-                },
-              ],
-            },
-            {
-              services: [
-                {
-                  price: 456,
-                },
-              ],
-            },
-          ],
-          coupons: [
-            {
-              priceDiscount: 1333,
-            },
-          ],
+      listing: {
+        metadata: {
+          pricingCurrency: 'USD',
         },
-      ],
+        item: {
+          price: 123,
+        },
+        shippingOptions: [
+          {
+            services: [
+              {
+                price: 123,
+              },
+              {
+                price: 234,
+              },
+            ],
+          },
+          {
+            services: [
+              {
+                price: 456,
+              },
+            ],
+          },
+        ],
+        coupons: [
+          {
+            priceDiscount: 1333,
+          },
+        ],
+      },
     });
 
     expect(parsed.item.price).to.equal(1.23);
@@ -108,43 +106,41 @@ describe('the Listing model', () => {
   it('converts BTC prices from Satoshi to BTC format in parse', () => {
     const listing = new Listing({}, { guid: '12345' });
     const parsed = listing.parse({
-      vendorListings: [
-        {
-          metadata: {
-            pricingCurrency: 'BTC',
-          },
-          item: {
-            price: 271453590,
-          },
-          shippingOptions: [
-            {
-              services: [
-                {
-                  price: 271453590,
-                },
-                {
-                  price: 873927651,
-                },
-              ],
-            },
-            {
-              services: [
-                {
-                  price: 281649276,
-                },
-              ],
-            },
-          ],
-          coupons: [
-            {
-              priceDiscount: 1333,
-            },
-            {
-              priceDiscount: 281649276,
-            },
-          ],
+      listing: {
+        metadata: {
+          pricingCurrency: 'BTC',
         },
-      ],
+        item: {
+          price: 271453590,
+        },
+        shippingOptions: [
+          {
+            services: [
+              {
+                price: 271453590,
+              },
+              {
+                price: 873927651,
+              },
+            ],
+          },
+          {
+            services: [
+              {
+                price: 281649276,
+              },
+            ],
+          },
+        ],
+        coupons: [
+          {
+            priceDiscount: 1333,
+          },
+          {
+            priceDiscount: 281649276,
+          },
+        ],
+      },
     });
 
     expect(parsed.item.price).to.equal(2.71453590);
@@ -187,7 +183,7 @@ describe('the Listing model', () => {
     const valErr = listing.validationError;
 
     expect(valErr && valErr.termsAndConditions &&
-      !!valErr.termsAndConditions.length || false).to.equal(true);
+        !!valErr.termsAndConditions.length || false).to.equal(true);
   });
 
   it('fails validation if the terms and conditions exceed the maximum length', () => {
@@ -202,83 +198,86 @@ describe('the Listing model', () => {
     const valErr = listing.validationError;
 
     expect(valErr && valErr.termsAndConditions &&
-      !!valErr.termsAndConditions.length || false).to.equal(true);
+        !!valErr.termsAndConditions.length || false).to.equal(true);
   });
 
   it('fails validation if, for a physical good, at least one shipping options is not provided',
-    () => {
-      const listing = new Listing();
+      () => {
+        const listing = new Listing();
 
-      listing.get('metadata').set('contractType', 'PHYSICAL_GOOD');
+        listing.get('metadata').set('contractType', 'PHYSICAL_GOOD');
 
-      listing.set({
-        shippingOptions: [],
-      }, { validate: true });
+        listing.set({
+          shippingOptions: [],
+        }, { validate: true });
 
-      const valErr = listing.validationError;
+        const valErr = listing.validationError;
 
-      expect(valErr && valErr.shippingOptions &&
-        !!valErr.shippingOptions.length || false).to.equal(true);
-    });
+        expect(valErr && valErr.shippingOptions &&
+            !!valErr.shippingOptions.length || false).to.equal(true);
+      });
 
   it('fails validation if the coupon count exceeds the maximum allowable amount',
-    () => {
-      const listing = new Listing();
-      const couponData = [];
+      () => {
+        const listing = new Listing();
+        const couponData = [];
 
-      for (let i = 0; i < (listing.max.couponCount + 1); i++) {
-        couponData.push({
-          discountCode: Date.now() + Math.random(),
-          percentDiscount: 10,
-        });
-      }
+        for (let i = 0; i < (listing.max.couponCount + 1); i++) {
+          couponData.push({
+            discountCode: Date.now() + Math.random(),
+            percentDiscount: 10,
+          });
+        }
 
-      listing.set({
-        coupons: couponData,
-      }, { validate: true });
+        listing.set({
+          coupons: couponData,
+        }, { validate: true });
 
-      const valErr = listing.validationError;
+        const valErr = listing.validationError;
 
-      expect(valErr && valErr.coupons &&
-        !!valErr.coupons.length || false).to.equal(true);
-    });
+        expect(valErr && valErr.coupons &&
+            !!valErr.coupons.length || false).to.equal(true);
+      });
 
   it('fails validation if a coupon price exceeds the listing price',
-    () => {
-      const listing = new Listing();
+      () => {
+        const listing = new Listing();
 
-      listing.set({
-        item: {
-          price: 500,
-        },
-        coupons: [
-          {
-            discountCode: Date.now() + Math.random(),
-            priceDiscount: 500, // should not fail validation
+        listing.set({
+          item: {
+            price: 500,
           },
-          {
-            discountCode: Date.now() + Math.random(),
-            priceDiscount: 501, // should fail validation
-          },
-          {
-            discountCode: Date.now() + Math.random(),
-            priceDiscount: 1500, // should fail validation
-          },
-        ],
-      }, { validate: true });
+          coupons: [
+            {
+              discountCode: Date.now() + Math.random(),
+              priceDiscount: 500, // should not fail validation
+            },
+            {
+              discountCode: Date.now() + Math.random(),
+              priceDiscount: 501, // should fail validation
+            },
+            {
+              discountCode: Date.now() + Math.random(),
+              priceDiscount: 1500, // should fail validation
+            },
+          ],
+        }, { validate: true });
 
-      const valErr = listing.validationError;
-      const coupons = listing.get('coupons');
+        const valErr = listing.validationError;
+        const coupons = listing.get('coupons');
 
-      expect(valErr && valErr[`coupons[${coupons.at(0).cid}].priceDiscount`] &&
-        !!valErr[`coupons[${coupons.at(0).cid}].priceDiscount`].length || false).to.equal(false);
+        expect(valErr && valErr[`coupons[${coupons.at(0).cid}].priceDiscount`] &&
+            !!valErr[`coupons[${coupons.at(0).cid}].priceDiscount`].length || false)
+            .to.equal(false);
 
-      expect(valErr && valErr[`coupons[${coupons.at(1).cid}].priceDiscount`] &&
-        !!valErr[`coupons[${coupons.at(1).cid}].priceDiscount`].length || false).to.equal(true);
+        expect(valErr && valErr[`coupons[${coupons.at(1).cid}].priceDiscount`] &&
+            !!valErr[`coupons[${coupons.at(1).cid}].priceDiscount`].length || false)
+            .to.equal(true);
 
-      expect(valErr && valErr[`coupons[${coupons.at(2).cid}].priceDiscount`] &&
-        !!valErr[`coupons[${coupons.at(2).cid}].priceDiscount`].length || false).to.equal(true);
-    });
+        expect(valErr && valErr[`coupons[${coupons.at(2).cid}].priceDiscount`] &&
+            !!valErr[`coupons[${coupons.at(2).cid}].priceDiscount`].length || false)
+            .to.equal(true);
+      });
 
   // todo: figure out how to stub BaseModel.sync so we could test conversion
   // of prices from integers to decimals in sync
