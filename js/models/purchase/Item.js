b73a74d278990dc26fcd7c8b69ddfb3dd7dddb1d
Josh Jeffryes <jjeffryes@gmail.com>
2017-05-22 14:06:23 -0400

    Fix error in receipt if quantity is not a number, change model test to isInteger.

--- js/models/purchase/Item.js
+++ js/models/purchase/Item.js
@@ -42,7 +42,7 @@ export default class extends BaseModel {
 
     if (attrs.quantity === 'undefined') {
       addError('quantity', app.polyglot.t('orderModelErrors.mustHaveQuantity'));
-    } else if (typeof attrs.quantity !== 'number') {
+    } else if (!Number.isInteger(attrs.quantity)) {
       addError('quantity', app.polyglot.t('orderModelErrors.quantityMustBeNumber'));
     } else if (attrs.quantity < 1) {
       addError('quantity', app.polyglot.t('orderModelErrors.noItems'));
