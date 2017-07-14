f527597842b38bbe25c36cb42d204f16747e7e72
Josh Jeffryes <jjeffryes@gmail.com>
2017-07-14 17:06:05 -0400

    Changes for Alpha Testing - added some more data to doorbell - changed doorbell to be run from starrt.js - hid the NSFW, Visual Effects, and Theme elements in settings - bumped the milestone number in the milestone tag to 3 - bumped the version number in package.json to 2.0.5

--- js/start.js
+++ js/start.js
@@ -404,6 +404,44 @@ function onboardIfNeeded() {
   return onboardIfNeededDeferred.promise();
 }
 
+function addDoorbell() {
+  window.doorbellOptions = {
+    appKey: 'lscnduocsmcCDtvh4DCZ4iQhGuCXZy4iexy7bIRa6wa5MFocLkSSutFU3zOii5k8',
+    name: app.profile.get('name'),
+    email: app.profile.get('contactInfo').email,
+    properties: {
+      peerID: app.profile.id,
+      vendor: app.profile.get('vendor'),
+      contactInfo: app.profile.get('contactInfo'),
+    }
+  };
+  (function(w, d, t) {
+    let hasLoaded = false;
+    function l() {
+      if (hasLoaded) {
+        return;
+      }
+      hasLoaded = true;
+      window.doorbellOptions.windowLoaded = true;
+      let g = d.createElement(t);
+      g.id = 'doorbellScript';
+      g.type = 'text/javascript';
+      g.async = true;
+      g.src = 'https://embed.doorbell.io/button/4990?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g);
+    }
+    if (w.attachEvent) {
+      w.attachEvent('onload', l);
+    } else if (w.addEventListener) {
+      w.addEventListener('load', l, false);
+    } else {
+      l();
+    }
+    if (d.readyState === 'complete') {
+      l();
+    }
+  }(window, document, 'script'));
+}
+
  // let's start our flow - do we need onboarding?,
  // fetching app-wide models...
 function start() {
@@ -430,6 +468,8 @@ function start() {
     app.ownFollowers = new Followers(null, { type: 'followers' });
 
     app.walletBalance = new WalletBalance();
+
+    addDoorbell();
 
     onboardIfNeeded().done(() => {
       fetchStartupData().done(() => {
