6692eac22850da4c9d2304a295d6a2c7b8323259
Josh Jeffryes <jjeffryes@gmail.com>
2017-08-05 15:33:42 -0400

    Pull more data from the user for feedback.

--- js/utils/feedback.js
+++ js/utils/feedback.js
@@ -1,34 +1,43 @@
 import app from '../app';
-
-const setFeedback = function () {
-  const profile = app.profile ? app.profile.toJSON() : {};
-  const contactInfo = profile.contactInfo || {};
-
-  window.doorbellOptions = {
-    appKey: 'lscnduocsmcCDtvh4DCZ4iQhGuCXZy4iexy7bIRa6wa5MFocLkSSutFU3zOii5k8',
-    name: profile.name,
-    email: contactInfo.email,
-    properties: {
-      peerID: profile.peerID,
-      vendor: profile.vendor,
-      contactInfo,
-    },
-  };
-};
+import { version } from '../../package.json';
+const os = require('os');
+// TODO: make that an import above
 
 /*
  * Sets the options for the feedback tool
  */
 
 export function setFeedbackOptions() {
-  setFeedback();
+  const profile = app.profile ? app.profile.toJSON() : {};
+  const contactInfo = profile.contactInfo || {};
+  const sVer = app.settings && app.settings.get('version') || '';
+  const serverVersion = sVer.substring(sVer.lastIndexOf(':') + 1, sVer.lastIndexOf('/'));
+
+  window.doorbellOptions = {
+    appKey: 'lscnduocsmcCDtvh4DCZ4iQhGuCXZy4iexy7bIRa6wa5MFocLkSSutFU3zOii5k8',
+    name: profile.name || "name data missing",
+    email: contactInfo.email || "email data missing",
+    properties: {
+      peerID: profile.peerID || "peerID data missing",
+      vendor: profile.vendor || "vendor data missing",
+      clientVersion: version,
+      serverVersion,
+      contactInfo,
+      systemLanguage: navigator.language,
+      numberOfCPUs: os.cpus().length,
+      cpu: os.cpus()[0],
+      RAMtotal: ((os.totalmem())/1048576).toFixed(2),
+      RAMfree: ((os.freemem())/1048576).toFixed(2),
+    },
+  };
+  console.log(window.doorbellOptions)
 }
 
 /*
  * Adds the feedback tool to the page.
  */
 export function addFeedback() {
-  if (!window.doorbellOptions) setFeedback();
+  if (!window.doorbellOptions) setFeedbackOptions();
 
   (function (w, d, t) {
     let hasLoaded = false;
