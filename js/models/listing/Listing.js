f851f3e012e6c340449cd6e55ab9d5f290403aea
Rob Misiorowski <rmisio@yahoo.com>
2017-08-07 16:19:00 -0700

    converting between ALL and individual country names and vice versa in parse and sync

--- js/models/listing/Listing.js
+++ js/models/listing/Listing.js
@@ -1,6 +1,7 @@
 import _ from 'underscore';
 import is from 'is_js';
 import app from '../../app';
+import { getIndexedCountries } from '../../data/countries';
 import { events as listingEvents, shipsFreeToMe } from './';
 import { decimalToInteger, integerToDecimal } from '../../utils/currency';
 import BaseModel from '../BaseModel';
@@ -211,6 +212,14 @@ export default class extends BaseModel {
 
         // remove the hash
         delete options.attrs.hash;
+
+        // If all countries are individually provided as shipping regions, we'll send
+        // 'ALL' to the server.
+        options.attrs.shippingOptions.forEach(shipOpt => {
+          if (_.isEqual(Object.keys(getIndexedCountries()), shipOpt.regions)) {
+            shipOpt.regions = ['ALL'];
+          }
+        });
       } else {
         options.url = options.url ||
           app.getServerUrl(`ob/listing/${this.get('slug')}`);
@@ -294,6 +303,11 @@ export default class extends BaseModel {
                   .services[serviceIndex].price = 0;
               }
             });
+          }
+
+          if (shipOpt.regions && shipOpt.regions.length && shipOpt.regions[0] === 'ALL') {
+            parsedResponse.shippingOptions[shipOptIndex].regions =
+              Object.keys(getIndexedCountries());
           }
         });
       }
