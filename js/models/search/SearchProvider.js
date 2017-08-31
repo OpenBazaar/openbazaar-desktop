02659602983b0c1567dcfa5fe2882de41152eba9
Josh Jeffryes <jjeffryes@gmail.com>
2017-08-31 12:36:12 -0400

    Fix typo

--- js/models/search/SearchProvider.js
+++ js/models/search/SearchProvider.js
@@ -30,7 +30,7 @@ export default class extends BaseModel {
       errObj[fieldName] = errObj[fieldName] || [];
       errObj[fieldName].push(error);
     };
-    const urlTypes = options.urlTypes || ['search', 'listings', 'torsearch', 'torlistins'];
+    const urlTypes = options.urlTypes || ['search', 'listings', 'torsearch', 'torlistings'];
 
     if (attrs.name && is.not.string(attrs.name)) {
       addError('name', app.polyglot.t('searchProviderModelErrors.invalidName'));
