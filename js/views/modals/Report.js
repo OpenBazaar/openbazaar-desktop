edd65d347eefaa91597989a6bda9f02835fa2794
Josh Jeffryes <jjeffryes@gmail.com>
2017-10-17 16:44:30 -0400

    Complete server interaction

--- js/views/modals/Report.js
+++ js/views/modals/Report.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
+import app from '../../app';
 import loadTemplate from '../../utils/loadTemplate';
 import BaseModal from './BaseModal';
+import { openSimpleMessage } from './SimpleMessage';
 
 export default class extends BaseModal {
   constructor(options = {}) {
@@ -26,31 +28,54 @@ export default class extends BaseModal {
 
   events() {
     return {
+      'keyup .js-otherInput': 'onKeyupOtherInput',
       'click .js-submit': 'onClickSubmit',
       ...super.events(),
     };
+  }
+
+  onKeyupOtherInput() {
+    this.getCachedEl('#ReasonOther').prop('checked', true);
   }
 
   onClickSubmit() {
     const data = {};
     data.peerID = this.peerID;
     data.slug = this.slug;
-    data.reason = '';
+    const formData = this.getFormData();
+    data.reason = formData.reason === 'Other' ? formData.other : formData.reason;
+    this.setState({
+      reporting: true,
+    });
     $.ajax({
       url: this.url,
       data,
     })
-      .done(()=> {
+      .done(() => {
         this.trigger('submitted');
       })
-      .fail()
-      .always();
+      .fail((xhr) => {
+        console.log(xhr)
+        console.log(xhr.status)
+        console.log(xhr.status === 404)
+        let failReason = xhr.responseJSON && xhr.responseJSON.reason || '';
+        if (xhr.status === 404) failReason = app.polyglot.t('listingReport.error404');
+        openSimpleMessage(
+          app.polyglot.t('listingReport.errorTitle'),
+          failReason
+        );
+      })
+      .always(() => {
+        this.close();
+      });
   }
 
   render() {
-    console.log('render report')
     loadTemplate('modals/report.html', (t) => {
-      this.$el.html(t(this.options));
+      this.$el.html(t({
+        ...this.options,
+        ...this._state,
+      }));
 
       super.render();
     });
