bcb7b2b910a5e06bc76d62876c8689d3c6ba9c7f
Julian Meyer <julianmeyer2000@gmail.com>
2017-12-27 13:34:35 -0800

    Update version and dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function setup(grunt) {
         productName: 'OpenBazaar',
         name: 'openbazaar2',
         arch: 'amd64',
-        version: '2.0.18p',
+        version: '1.0.0',
         bin: 'openbazaar2',
         maintainer: 'OpenBazaar <project@openbazaar.org>',
         rename(dest) {
