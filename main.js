be9a7e9e16be4c7d5bd2d8bee30e0b5301a96d8d
Rob Misiorowski <rmisio@yahoo.com>
2017-12-29 15:58:47 -0800

    bouncing local server if the zcash binary path changes

--- main.js
+++ main.js
@@ -84,7 +84,8 @@ if (handleStartupEvent()) {
   console.log('OpenBazaar started on Windows...');
 }
 
-const serverPath = `${__dirname}${path.sep}..${path.sep}openbazaar-go${path.sep}`;
+// const serverPath = `${__dirname}${path.sep}..${path.sep}openbazaar-go${path.sep}`;
+const serverPath = '/Users/rob/develop/openbazaar/v2/server/';
 const serverFilename = process.platform === 'darwin' || process.platform === 'linux' ?
   'openbazaard' : 'openbazaard.exe';
 const isBundledApp = fs.existsSync(serverPath + serverFilename);
