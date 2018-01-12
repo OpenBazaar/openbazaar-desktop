87accb3bf01b352cc6745ceb48ab5006b94b1a74
Rob Misiorowski <rmisio@yahoo.com>
2018-01-11 20:37:07 -0800

    wired in some additional translations

--- js/start.js
+++ js/start.js
@@ -43,11 +43,6 @@ import WalletSetup from './views/modals/WalletSetup';
 import SearchProvidersCol from './collections/search/SearchProviders';
 import defaultSearchProviders from './data/defaultSearchProviders';
 
-import { block } from './utils/block';
-
-console.log('block');
-window.block = block;
-
 fixLinuxZoomIssue();
 handleServerShutdownRequests();
 
