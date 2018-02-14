e57ca7c6fabf9655a2e35fb1ae8bcad92fb6ebf8
Rob Misiorowski <rmisio@yahoo.com>
2018-02-14 09:50:18 -0800

    collapsing the three dispute related transaction filters into one

--- js/views/transactions/Transactions.js
+++ js/views/transactions/Transactions.js
@@ -182,7 +182,7 @@ export default class extends baseVw {
     return {
       search: '',
       sortBy: 'UNREAD',
-      states: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      states: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
     };
   }
 
@@ -208,7 +208,7 @@ export default class extends baseVw {
         text: app.polyglot.t('transactions.filters.fulfilled'),
         checked: this.salesPurchasesDefaultFilter.states.indexOf(3) > -1,
         className: 'filter',
-        targetState: [5],
+        targetState: [5, 13],
       },
       {
         id: 'filterRefunded',
@@ -218,25 +218,11 @@ export default class extends baseVw {
         targetState: [9],
       },
       {
-        id: 'filterDisputeOpen',
-        text: app.polyglot.t('transactions.filters.disputeOpen'),
-        checked: this.salesPurchasesDefaultFilter.states.indexOf(10) > -1,
+        id: 'filterDisputes',
+        text: app.polyglot.t('transactions.filters.disputes'),
+        checked: [10, 11, 12].includes(this.salesPurchasesDefaultFilter.states),
         className: 'filter',
-        targetState: [10],
-      },
-      {
-        id: 'filterDisputePending',
-        text: app.polyglot.t('transactions.filters.disputePending'),
-        checked: this.salesPurchasesDefaultFilter.states.indexOf(11) > -1,
-        className: 'filter',
-        targetState: [11],
-      },
-      {
-        id: 'filterDisputeClosed',
-        text: app.polyglot.t('transactions.filters.disputeClosed'),
-        checked: this.salesPurchasesDefaultFilter.states.indexOf(12) > -1,
-        className: 'filter',
-        targetState: [12],
+        targetState: [10, 11, 12],
       },
       {
         id: 'filterCompleted',
