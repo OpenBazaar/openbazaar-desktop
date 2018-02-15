0a359c44e07368d6d28f2a3f6721497371ba0135
Rob Misiorowski <rmisio@yahoo.com>
2018-02-14 17:35:34 -0800

    wip - implementing the Processing Error view

---
+++ js/views/modals/orderDetail/summaryTab/ProcessingError.js
@@ -0,0 +1,65 @@
+// import $ from 'jquery';
+import {
+  cancelingOrder,
+  cancelOrder,
+  events as orderEvents,
+} from '../../../../utils/order';
+import loadTemplate from '../../../../utils/loadTemplate';
+import BaseVw from '../../../baseVw';
+
+export default class extends BaseVw {
+  constructor(options = {}) {
+    if (!options.orderId) {
+      throw new Error('Please provide the order id.');
+    }
+
+    const opts = {
+      initialState: {
+        isBuyer: false,
+        isModerated: false,
+        cancelInProgress: cancelingOrder(options.orderId),
+        ...options.initialState || {},
+      },
+    };
+
+    super(opts);
+
+    this.orderId = options.orderId;
+
+    this.listenTo(orderEvents, 'cancelingOrder', e => {
+      if (e.id === this.orderId) {
+        this.setState({ cancelInProgress: true });
+      }
+    });
+
+    this.listenTo(orderEvents, 'cancelOrderComplete cancelOrderFail', e => {
+      if (e.id === this.orderId) {
+        this.setState({ cancelInProgress: false });
+      }
+    });
+  }
+
+  className() {
+    return 'clrTErr';
+  }
+
+  events() {
+    return {
+      'click .js-cancelOrder': 'onClickCancelOrder',
+    };
+  }
+
+  onClickCancelOrder() {
+    cancelOrder(this.orderId);
+  }
+
+  render() {
+    loadTemplate('modals/orderDetail/summaryTab/processingError.html', (t) => {
+      this.$el.html(t({
+        ...this._state,
+      }));
+    });
+
+    return this;
+  }
+}
