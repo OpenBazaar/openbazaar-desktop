62e5dc82a3209117db857e3c2b25cf77174229f4
Rob Misiorowski <rmisio@yahoo.com>
2018-06-14 16:50:07 -0700

    lint fix

--- bin/getCryptoData.js
+++ bin/getCryptoData.js
@@ -64,7 +64,8 @@ function getIcon(coin) {
 
   fetch(getIconUrl(coin.id))
     .then(res => {
-      const writeStream = fs.createWriteStream(`${iconOutputPath}${path.sep}${coin.symbol}-icon.png`);
+      const writeStream =
+        fs.createWriteStream(`${iconOutputPath}${path.sep}${coin.symbol}-icon.png`);
       writeStream.on('error', err => {
         logError(`There was an error writing the icon for symbol ${coin.symbol},` +
           ` with an id of ${coin.id}: ${err}`);
