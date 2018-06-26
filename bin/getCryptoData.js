4ac115106841d6d84bc735246726d5245fa10050
Rob Misiorowski <rmisio@yahoo.com>
2018-06-25 17:10:08 -0700

    updating the crypto script to use the whitelist from the ticker

--- bin/getCryptoData.js
+++ bin/getCryptoData.js
@@ -9,15 +9,24 @@ const iconOutputPath = `${__dirname}${path.sep}..${path.sep}imgs${path.sep}crypt
 const translationFile = `${__dirname}${path.sep}..${path.sep}js${path.sep}` +
   `languages${path.sep}/en_US.json`;
 
-// In the event of duplicate crypto currency codes the CMC id listed here
-// will be used. For dupes not listed here, the first one in the results
-// set obtained from CMC will be used (which is likely the one that was
-// listed with CMC the longest).
-const whitelist = {
-  BTG: 2083,
-};
-
 draftLog(console);
+
+let whitelist = {};
+
+/*
+ * The whitelist is obtained from the ticker. It is a mapping of currency code
+ * to CMC id. In the event of duplicate crypto currency codes the CMC id listed in
+ * the whitelist will be used. For dupes not listed, the first one in the results
+ * set obtained from CMC will be used (which is likely the one that was
+ * listed with CMC the longest).
+ */
+function getWhitelist() {
+  return new Promise((resolve, reject) => {
+    fetch('https://ticker.openbazaar.org/whitelist/')
+      .then(res => resolve(res.json()))
+      .catch(err => reject(err));
+  });
+}
 
 function getCoinList() {
   return new Promise((resolve, reject) => {
@@ -83,76 +92,87 @@ function getIcon(coin) {
     });
 }
 
-draft('Obtaining crypto icons...');
+console.log('Obtaining whitelist...');
 
-getCoinList()
-  .then(results => {
-    // remove duplicates from the data set using our whitelist
-    const newList = [];
-    results.data.forEach((c, i) => {
-      const furtherResults = results.data.slice(i + 1);
-      if (
-        !(whitelist[c.symbol] && whitelist[c.symbol] !== c.id) &&
-        !furtherResults.find(coin => coin.symbol === c.symbol)
-      ) {
-        newList.push(c);
-      }
-    });
-    totalCoins = newList.length;
-    results.data = newList;
+getWhitelist()
+  .then(
+    wl => {
+      whitelist = wl;
+      console.log('Whitelist obtained...');
+      draft('Obtaining crypto icons...');
 
-    results.data.sort((a, b) => {
-      if (a.symbol < b.symbol) {
-        return -1;
-      }
-      if (a.symbol > b.symbol) {
-        return 1;
-      }
+      getCoinList()
+        .then(results => {
+          // remove duplicates from the data set using our whitelist
+          const newList = [];
+          results.data.forEach((c, i) => {
+            const furtherResults = results.data.slice(i + 1);
+            if (
+              !(whitelist[c.symbol] && whitelist[c.symbol] !== c.id) &&
+              !furtherResults.find(coin => coin.symbol === c.symbol)
+            ) {
+              newList.push(c);
+            }
+          });
+          totalCoins = newList.length;
+          results.data = newList;
 
-      return 0;
-    });
+          results.data.sort((a, b) => {
+            if (a.symbol < b.symbol) {
+              return -1;
+            }
+            if (a.symbol > b.symbol) {
+              return 1;
+            }
 
-    const callGetIcon = (index = 0) => {
-      getIcon(results.data[index]);
-      if (index + 1 < results.data.length) {
-        // Give some space between requests, otherwise kicking off so many at once
-        // leads to many of them failing.
-        setTimeout(() => callGetIcon(index + 1), 100);
-      }
-    };
-    callGetIcon();
+            return 0;
+          });
 
-    if (results.data.length) {
-      console.log('Updating the en_US.json translation file...');
-      fs.readFile(translationFile, (err, data) => {
-        if (err) {
-          logError(`There was an error processing the translation file: ${err}`);
-          return;
-        }
+          const callGetIcon = (index = 0) => {
+            getIcon(results.data[index]);
+            if (index + 1 < results.data.length) {
+              // Give some space between requests, otherwise kicking off so many at once
+              // leads to many of them failing.
+              setTimeout(() => callGetIcon(index + 1), 50);
+            }
+          };
+          callGetIcon();
 
-        const parsed = JSON.parse(data);
-        results.data.forEach(c => {
-          if (whitelist[c.symbol] && whitelist[c.symbol] !== c.id) return;
-          parsed.cryptoCurrencies[c.symbol] = c.name;
-        });
-        const jsonConfig = {
-          type: 'space',
-          size: 2,
-        };
-        fs.writeFile(translationFile, jsonformat(parsed, jsonConfig), writeErr => {
-          if (err) {
-            logError(`There was an error writing the translation file: ${writeErr}`);
-            return;
+          if (results.data.length) {
+            console.log('Updating the en_US.json translation file...');
+            fs.readFile(translationFile, (err, data) => {
+              if (err) {
+                logError(`There was an error processing the translation file: ${err}`);
+                return;
+              }
+
+              const parsed = JSON.parse(data);
+              results.data.forEach(c => {
+                if (whitelist[c.symbol] && whitelist[c.symbol] !== c.id) return;
+                parsed.cryptoCurrencies[c.symbol] = c.name;
+              });
+              const jsonConfig = {
+                type: 'space',
+                size: 2,
+              };
+              fs.writeFile(translationFile, jsonformat(parsed, jsonConfig), writeErr => {
+                if (err) {
+                  logError(`There was an error writing the translation file: ${writeErr}`);
+                  return;
+                }
+
+                console.log(`Successfully wrote to ${translationFile}`);
+              });
+            });
           }
-
-          console.log(`Successfully wrote to ${translationFile}`);
-        });
-      });
-    }
-  }).catch(err => {
-    if (err.type === 'system') {
-      logError(`There was an error fetching the coin list: ${err}`);
-    } else {
-      logError(err);
-    }
-  });
+        })
+        .catch(err => {
+          if (err.type === 'system') {
+            logError(`There was an error fetching the coin list: ${err}`);
+          } else {
+            logError(err);
+          }
+        });      
+    },
+    err => logError(`There was an error fetching the coin list: ${err}`),
+  );
