e0e8a7623dcc0be8f3eca09b0836ff918df04b8e
jjeffryes <jjeffryes@gmail.com>
2018-09-24 15:38:40 -0400

    Code Cleanup

--- js/views/modals/about/Donations.js
+++ js/views/modals/about/Donations.js
@@ -1,18 +1,11 @@
-import { isSupportedWalletCur } from '../../../data/walletCurrencies';
-import { getWallet, launchWallet } from '../../../utils/modalManager';
-import { openSimpleMessage } from '../../modals/SimpleMessage';
-import loadTemplate from '../../../utils/loadTemplate';
-import baseVw from '../../baseVw';
 import { clipboard } from 'electron';
 import qr from 'qr-encode';
+import { isSupportedWalletCur } from '../../../data/walletCurrencies';
+import { getWallet, launchWallet } from '../../../utils/modalManager';
+import loadTemplate from '../../../utils/loadTemplate';
+import baseVw from '../../baseVw';
 
 
-
-// const obDonationAddress = '3EN8kP3yW9MGvyiRMtGqKavQ9wYg4BspzR';
-// const qrCodeDataURI = qr(`bitcoin:${obDonationAddress}`, { type: 6, size: 6, level: 'Q' });
-const btcAddress = '3EN8kP3yW9MGvyiRMtGqKavQ9wYg4BspzR';
-const bchAddress = 'foo';
-const donationCountFloor = 500;
 let hiderTimer;
 
 export default class extends baseVw {
@@ -31,16 +24,21 @@ export default class extends baseVw {
     });
     this.options = opts;
 
-    this.donationAddresses = {
+    const btcAddress = '3EN8kP3yW9MGvyiRMtGqKavQ9wYg4BspzR';
+    const bchAddress = 'prws8awqjg8l497x3h7qeu6jps4fdmk75vhlng8akj';
+
+    this.dCoins = {
       BTC: {
-        address: btcAddress,
+        obDonationAddress: btcAddress,
         qrCodeDataURI: qr(`bitcoin:${btcAddress}`,
           { type: 6, size: 6, level: 'Q' }),
+        supported: isSupportedWalletCur('BTC'),
       },
       BCH: {
-        address: bchAddress,
+        obDonationAddress: bchAddress,
         qrCodeDataURI: qr(`bitcoin:${bchAddress}`,
           { type: 6, size: 6, level: 'Q' }),
+        supported: isSupportedWalletCur('BCH'),
       }
     };
   }
@@ -55,52 +53,41 @@ export default class extends baseVw {
   }
 
   showBTC() {
-    this.setState({ showCoin:'BTC'});
+    this.setState({ showCoin: 'BTC' });
   };
 
   showBCH() {
-    this.setState({ showCoin: 'BCH'});
+    this.setState({ showCoin: 'BCH' });
   }
 
   copyDonationAddress() {
-    addr = this.donationAddresses[this.getState().showCoin].address;
+    const addr = this.dCoins[this.getState().showCoin].obDonationAddress;
     clipboard.writeText(addr);
 
-    this.$copyNotification.addClass('active');
+    this.getCachedEl('.js-copyNotification').addClass('active');
     if (!!hiderTimer) {
       clearTimeout(hiderTimer);
     }
     hiderTimer = setTimeout(
-      () => this.$copyNotification.removeClass('active'), 3000);
-  }
-
-  get $copyNotification() {
-    return this._$copyNotification ||
-      (this._$copyNotification = this.$('.js-copyNotification'));
+      () => this.getCachedEl('.js-copyNotification')
+        .removeClass('active'), 3000);
   }
 
   openInWalletClick() {
-    const coin = this.getState().showCoin;
-    // This is very unlikely to happen unless the user manually disabled some of
-    // their wallets.
-    if (!isSupportedWalletCur(coin)) {
-      openSimpleMessage(app.polyglot.t('about.donationsTab.unableToOpenInWallet.title'),
-        app.polyglot.t('about.donationsTab.unableToOpenInWallet.body'));
-      return;
-    }
     const wallet = getWallet();
     if (!wallet) {
       launchWallet({
         initialActiveCoin: this.getState().showCoin,
         // TODO: add send address here
       })
-        //.setSendFormData({ address: obDonationAddress });
     } else {
       /*
       * Do we still need a check to see if the wallet is in the middle of
       * sending a payment here? If so, show this error message:
-      * openSimpleMessage(app.polyglot.t('about.donationsTab.unableToOpenInWallet.title'),
-       app.polyglot.t('about.donationsTab.unableToOpenInWallet.body'));
+      * openSimpleMessage(
+      * app.polyglot.t('about.donationsTab.unableToOpenInWallet.title'),
+      * app.polyglot.t('about.donationsTab.unableToOpenInWallet.body'));
+      * The phrases will need to be added back to en_US.json
       * */
       wallet.activeCoin = this.getState().showCoin;
       // TODO: add send address here
@@ -109,13 +96,12 @@ export default class extends baseVw {
   }
 
   render() {
+    super.render();
     const showCoin = this.getState().showCoin;
     loadTemplate('modals/about/donations.html', (t) => {
       this.$el.html(t({
-        obDonationAddress: this.donationAddresses[showCoin].address,
-        qrCodeDataURI: this.donationAddresses[showCoin].qrCodeDataURI,
-        donationCountFloor,
         showCoin,
+        ...this.dCoins[showCoin],
       }));
     });
 
