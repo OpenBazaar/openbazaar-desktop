58a7dbe2149641fb2e939985fe3982f3f0bb630e
Rob Misiorowski <rmisio@yahoo.com>
2018-11-12 10:48:56 -0600

    removing code dealing with spcifying the zcash binary

--- js/utils/serverConnect.js
+++ js/utils/serverConnect.js
@@ -333,28 +333,10 @@ export default function connect(server, options = {}) {
       ' the bundled app.');
   }
 
-  const curLocalServerCoin = serverStartArgsToCoin();
-  let curLocalServerZecBinaryPath = null;
-
-  if (localServer) {
-    if (curLocalServerCoin === 'ZEC') {
-      const zecIndex = localServer.lastStartCommandLineArgs
-        .indexOf(serverCurStartArgMap.ZEC);
-
-      if (zecIndex !== -1 &&
-        typeof localServer.lastStartCommandLineArgs[zecIndex + 1] === 'string') {
-        curLocalServerZecBinaryPath = localServer.lastStartCommandLineArgs[zecIndex + 1];
-      }
-    }
-  }
-
   // If we're not connecting to the local bundled server or it's running with incompatible
   // command line arguments, let's ensure it's stopped.
   if (localServer && localServer.isRunning &&
-    (
-      !server.get('builtIn') || serverCurrency !== serverStartArgsToCoin() ||
-      (serverCurrency === 'ZEC' && server.get('zcashBinaryPath') !== curLocalServerZecBinaryPath)
-    )) {
+    (!server.get('builtIn') || serverCurrency !== serverStartArgsToCoin())) {
     deferred.notify({ status: 'stopping-local-server' });
     localServer.stop();
 
@@ -460,10 +442,6 @@ export default function connect(server, options = {}) {
 
               if (serverCoinArg) {
                 commandLineArgs.push(serverCoinArg);
-
-                if (serverCurrency === 'ZEC') {
-                  commandLineArgs.push(server.get('zcashBinaryPath'));
-                }
               }
             }
 
