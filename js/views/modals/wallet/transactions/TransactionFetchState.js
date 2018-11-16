f006048d73cf4ed72de0b1ad9be7b62f177ce768
Rob Misiorowski <rmisio@yahoo.com>
2018-11-16 13:10:07 -0600

    fixing issues in the error and retry state of the wallet transactions

--- js/views/modals/wallet/transactions/TransactionFetchState.js
+++ js/views/modals/wallet/transactions/TransactionFetchState.js
@@ -1,14 +1,9 @@
-import _ from 'underscore';
 import loadTemplate from '../../../../utils/loadTemplate';
 import BaseVw from '../../../baseVw';
 
 export default class extends BaseVw {
   constructor(options = {}) {
     super(options);
-
-    this._state = {
-      ...options.initialState || {},
-    };
   }
 
   className() {
@@ -25,31 +20,10 @@ export default class extends BaseVw {
     this.trigger('clickRetryFetch');
   }
 
-  getState() {
-    return this._state;
-  }
-
-  setState(state, replace = false) {
-    let newState;
-
-    if (replace) {
-      this._state = {};
-    } else {
-      newState = _.extend({}, this._state, state);
-    }
-
-    if (!_.isEqual(this._state, newState)) {
-      this._state = newState;
-      this.render();
-    }
-
-    return this;
-  }
-
   render() {
     loadTemplate('modals/wallet/transactions/transactionFetchState.html', (t) => {
       this.$el.html(t({
-        ...this._state,
+        ...this.getState(),
       }));
     });
 
