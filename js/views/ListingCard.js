c47fbae6e8d3460b5ed1b73d38e2101eee3dd940
jjeffryes <jjeffryes@gmail.com>
2019-02-11 11:04:28 -0500

    change avatarHashes to avatar
    The listingShort model doesn't have an avatarHashes object inside it's vendor object, it has an avatar object.

--- js/views/ListingCard.js
+++ js/views/ListingCard.js
@@ -134,9 +134,9 @@ export default class extends baseVw {
     this.listingImage.src = listingImageSrc;
 
     const vendor = this.model.get('vendor');
-    if (vendor && vendor.avatarHashes) {
+    if (vendor && vendor.avatar) {
       const avatarImageSrc = app.getServerUrl(
-        `ob/images/${isHiRez() ? vendor.avatarHashes.small : vendor.avatarHashes.tiny}`
+        `ob/images/${isHiRez() ? vendor.avatar.small : vendor.avatar.tiny}`
       );
 
       this.avatarImage = new Image();
