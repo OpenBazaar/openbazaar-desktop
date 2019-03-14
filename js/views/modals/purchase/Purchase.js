382bc985be6c0703f25049305d37cfe1e82e637e
jjeffryes <jjeffryes@gmail.com>
2019-03-14 13:32:39 -0400

    code review change

--- js/views/modals/purchase/Purchase.js
+++ js/views/modals/purchase/Purchase.js
@@ -310,7 +310,7 @@ export default class extends BaseModal {
 
   setModelQuantity(quantity, cur = this.cryptoAmountCurrency) {
     if (this.listing.isCrypto && (typeof cur !== 'string' || !cur)) {
-      throw new Error('Please provide the currency code as a string.');
+      throw new Error('Please provide the currency code as a valid, non-empty string.');
     }
 
     let mdQuantity = quantity;
