7c3cf32e2b3cebdb0fd4c5564d660ff6175723d4
jjeffryes <j1584275+jjeffryes@users.noreply.github.com>
2019-09-04 11:36:49 -0400

    update notification types
    Update to match https://github.com/OpenBazaar/openbazaar-go/blob/6eca0f933482c206aee7b124137baa76532faad3/repo/constants.go

--- js/collections/Notifications.js
+++ js/collections/Notifications.js
@@ -77,7 +77,7 @@ export function getNotifDisplayData(attrs, options = {}) {
     text = app.polyglot.t('notifications.text.orderConfirmation', {
       vendorName,
     });
-  } else if (attrs.type === 'declined') {
+  } else if (attrs.type === 'orderDeclined') {
     const vendorName = opts.native ?
       getName(attrs.vendorHandle, attrs.vendorId) :
       `<a class="clrTEm" href="#${attrs.vendorId}">` +
@@ -86,7 +86,7 @@ export function getNotifDisplayData(attrs, options = {}) {
     text = app.polyglot.t('notifications.text.declined', {
       vendorName,
     });
-  } else if (attrs.type === 'canceled') {
+  } else if (attrs.type === 'cancel') {
     const buyerName = opts.native ?
       getName(attrs.buyerHandle, attrs.buyerId) :
       `<a class="clrTEm" href="#${attrs.buyerId}">` +
