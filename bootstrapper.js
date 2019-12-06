55b713653d5f32444ef195df27d2c072ebda1800
Josh Jeffryes <1584275+jjeffryes@users.noreply.github.com>
2019-12-06 13:21:01 -0500

    Downgrade babel to prevent windows browsersync looping.

--- bootstrapper.js
+++ bootstrapper.js
@@ -1,6 +1,6 @@
 // install babel hooks in the main process
 if (process.env.NODE_ENV === 'development') {
-  require('@babel/register');  // eslint-disable-line global-require
+  require('babel-register');  // eslint-disable-line global-require
   require('./main.js'); // eslint-disable-line global-require
 } else {
   require('./mainBuilt.js'); // eslint-disable-line global-require, import/no-unresolved
